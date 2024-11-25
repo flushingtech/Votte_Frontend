07575188410a8008abb21e537f8a72ffb638fa28
Mahmoud <tkhattab1999@gmail.com>
2024-11-25 17:50:12 -0500

    fix(app.jsx): route to ideas from event

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Landing from './pages/Landing';
 import Home from './pages/Home';
 import AdminPage from './pages/Admin';
-import IdeasForEvent from './components/admin/IdeasForEvent';
+import EventScreen from './pages/EventScreen';
+import IdeasForEvent from './components/admin/IdeasForEvent'; // Admin Event Screen
 import { checkAdminStatus } from './api/API';
 
 const getUserEmail = () => {
@@ -39,18 +40,30 @@ function App() {
     <GoogleOAuthProvider clientId={`${import.meta.env.VITE_GOOGLE_CLIENT_ID}`}>
       <Router>
         <Routes>
+          {/* Landing page */}
           <Route path="/" element={<Landing />} />
+
+          {/* Regular user routes */}
           <Route path="/home" element={<Home />} />
-          <Route path="/admin" element={
-            <RequireAdmin userEmail={userEmail}>
-              <AdminPage />
-            </RequireAdmin>
-          } />
-          <Route path="/admin/event/:eventId" element={
-            <RequireAdmin userEmail={userEmail}>
-              <IdeasForEvent />
-            </RequireAdmin>
-          } />
+          <Route path="/event/:eventId" element={<EventScreen />} />
+
+          {/* Admin routes */}
+          <Route
+            path="/admin"
+            element={
+              <RequireAdmin userEmail={userEmail}>
+                <AdminPage />
+              </RequireAdmin>
+            }
+          />
+          <Route
+            path="/admin/event/:eventId"
+            element={
+              <RequireAdmin userEmail={userEmail}>
+                <IdeasForEvent userEmail={userEmail} />
+              </RequireAdmin>
+            }
+          />
         </Routes>
       </Router>
     </GoogleOAuthProvider>
