35111b000a41b03318b27b5f277a8114c7524c3f
benhuangbmj <benjaminwong1985@gmail.com>
2025-05-01 11:28:56 -0400

    chore: simplify the tailwind css rules

--- src/components/MarkdownPreviewer.jsx
+++ src/components/MarkdownPreviewer.jsx
@@ -4,6 +4,17 @@ import PropTypes from "prop-types";
 
 export default function MarkdownPreviewer({ children, text }) {
   const [previewPressed, setPreviewPressed] = React.useState(false);
+  const markdownRef = React.useRef(null);
+  React.useEffect(() => {
+    if (text) {
+      const lines = text.split("\n");
+      console.log("lines", lines);
+      console.log(markdownRef.current);
+      if (markdownRef.current) {
+        console.log("markdown ref", markdownRef.current.children);
+      }
+    }
+  }, [text]);
   return (
     <>
       <div className="border border-gray-500 text-left">
@@ -11,8 +22,8 @@ export default function MarkdownPreviewer({ children, text }) {
           type="button"
           className={`py-1 px-2 bg-gray-700 text-gray-400 border-2 ${
             previewPressed
-              ? "border-t-[#808080] border-r-[#dfdfdf] border-b-[#dfdfdf] border-l-[#808080] shadow-inner"
-              : "border-t-[#dfdfdf] border-r-[#808080] border-b-[#808080] border-l-[#dfdfdf] shadow"
+              ? "border-t-gray-500 border-r-gray-300 border-b-gray-300 border-l-gray-500 shadow-[-1px_-1px_0px_gray]"
+              : "border-t-gray-300 border-r-gray-500 border-b-gray-500 border-l-gray-300 shadow-[1px_1px_0px_gray]"
           } cursor-pointer`}
           onClick={() => setPreviewPressed((prev) => !prev)}
         >
@@ -26,7 +37,7 @@ export default function MarkdownPreviewer({ children, text }) {
       {previewPressed && (
         <div className="w-full h-64 max-h-[25vh] min-h-24 px-3 border border-gray-500 bg-gray-700 text-white shadow-sm overflow-auto">
           <span className="font-bold">Markdown Preview</span>
-          <div className="markdown text-left text-xs">
+          <div ref={markdownRef} className="markdown text-left text-xs">
             <Markdown>{text}</Markdown>
           </div>
         </div>
